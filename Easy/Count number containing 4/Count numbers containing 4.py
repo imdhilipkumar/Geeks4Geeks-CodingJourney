1ee5495c4caf334a532661786bdb80ef014c87b5
Dhilip Kumar <65275469+imdhilipkumar@users.noreply.github.com>
2024-06-13 20:46:07 +0530

    Delete Easy/Count number containing 4 directory

--- Easy/Count number containing 4/Count numbers containing 4.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countNumberswith4(self, n : int) -> int:
-        # code here
-        count = 0
-        for i in range(0, n+1):
-            if "4" in str(i):
-                count+=1
-        return count
-                
